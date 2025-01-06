892448faec0c55b649cee8e6d5fcea5543cd50a7
rafagar96 <rafagar96@gmail.com>
2025-01-06 12:14:08 +0000

    Added Pagination component

--- src/components/ComicsOverview.vue
+++ src/components/ComicsOverview.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import type { Ref } from 'vue';
+import { useRoute } from 'vue-router';
 
 import { useComics } from "@/composables/marvelApi";
 import type { Comic } from '@/types/marvel';
 
 import LoadingIndicator from '@/components/LoadingIndicator.vue';
+import ComicCard from './ComicCard.vue';
+import Pagination from './Pagination.vue';
+
+const route = useRoute();
 
 const isLoading: Ref<boolean> = ref(false);
 const data: Ref<Comic[] | undefined > = ref();
+const currentPage: Ref<number | string> = ref(0);
+const totalPages: Ref<number> = ref(0);
+
+if (route.params.page) {
+  currentPage.value = +route.params.page;
+}
 
 const getComics = async () => {
   isLoading.value = true;
   const comics = await useComics();
+
+  currentPage.value = comics?.offset / comics?.limit || 0;
+  totalPages.value = Math.ceil(comics?.total / comics?.limit)
 
   data.value = comics.results;
   isLoading.value = false
@@ -28,8 +42,9 @@ onMounted(async () => {
     <LoadingIndicator v-if="isLoading" text="Loading comics..." />
     <div v-if="data && !isLoading">
       <div class="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <div :key="comic.id" v-for='comic in data'>{{  comic.title }}</div>
+        <ComicCard :comic="comic" :key="comic-id" v-for="comic in data"></ComicCard>
       </div>
+      <Pagination :total-pages="totalPages" path="/" :current-page="+currentPage"></Pagination>
     </div>
   </div>
 </template>
